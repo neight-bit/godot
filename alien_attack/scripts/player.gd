61fd1c954f8a8c711dbebcea92b34b412ea624fc
neight <ellis.nathaniel@gmail.com>
2024-04-29 22:04:15 -0500

    Add player clamp

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var speed = 350
 
 func _physics_process(delta):
-	
+
 	velocity = Vector2(0, 0)
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
@@ -13,4 +13,8 @@ func _physics_process(delta):
 		velocity.y = speed
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -speed
+
+	var screen_size = get_viewport_rect().size
+	
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
 	move_and_slide()
