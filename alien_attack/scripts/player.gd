d654ae55df9c4cfba2b2b4f2a64b7e1e3349c70e
neight <ellis.nathaniel@gmail.com>
2024-04-29 23:09:44 -0500

    add rocket function

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 var speed = 350
 
+var rocket_scene = preload("res://scenes/rocket.tscn")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
 func _physics_process(delta):
 
 	velocity = Vector2(0, 0)
@@ -13,8 +19,13 @@ func _physics_process(delta):
 		velocity.y = speed
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -speed
-
+	
 	var screen_size = get_viewport_rect().size
 	
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
 	move_and_slide()
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 50
