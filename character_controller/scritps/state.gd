32dd476c2fba860b4f2e22d31c0b1ae94fa92aef
neight.bit <neight.bit@protonmail.com>
2024-11-30 23:18:12 -0600

    Add state_machine project

---
+++ character_controller/scritps/state.gd
@@ -0,0 +1,24 @@
+class_name State
+extends Node
+
+@export
+var animation_name: String
+@export
+var move_speed: float = 300
+
+var parent: Player
+
+func enter() -> void:
+	parent.animations.play(animation_name)
+
+func exit() -> void:
+	pass
+	
+func process_input(event: InputEvent) -> State:
+	return null
+
+func process_physics(delta: float) -> State:
+	return null
+
+func process_frame(delta: float) -> State:
+	return null
