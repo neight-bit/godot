874861eb310efb72fc1bb80abb5bd4a2d23ea18d
neight <ellis.nathaniel@gmail.com>
2025-02-05 22:27:24 -0600

    Fully move physical input into dedicated component, create new actor entities

--- character_controller/components/component_scripts/attack_component.gd
+++ character_controller/components/component_scripts/attack_component.gd
@@ -6,21 +6,26 @@ var cancelable_attacks: Dictionary = {
 	"airborne_attack": false,
 }
 
+var wants_attack: bool = false
+
 func _ready():
 	print("initializing attack component")
 	actions = [
+		["get_wants_attack",	self, {}],
+		["set_wants_attack",	self, {'value': false}],
 		["get_attack",			self, {}],
 		["wants_attack",		self, {}],
 		["can_cancel_attack",	self, {"attack_name": ""}],
 	]
 
-func wants_attack() -> bool:
-	if Input.is_action_just_pressed("attack"):
-		return true
-	return false
+func set_wants_attack(value: bool) -> void:
+	wants_attack = value
+
+func get_wants_attack() -> bool:
+	return wants_attack
 
 func get_attack() -> bool:
-	return wants_attack()
+	return wants_attack
 	
 func can_cancel_attack(attack_name: String) -> bool:
 	return cancelable_attacks.get(attack_name, false)
--- character_controller/components/component_scripts/input_component.gd
+++ character_controller/components/component_scripts/input_component.gd
@@ -4,6 +4,7 @@ func _ready():
 	actions = []
 
 func _unhandled_input(event: InputEvent) -> void:
-	mediator.request("set_movement_direction", [Input.get_axis('move_left', 'move_right')])
-	mediator.request("set_wants_jump", [Input.is_action_just_pressed('jump')])
-	mediator.request("set_wants_dash", [Input.is_action_just_pressed('dash')])
+	mediator.request("set_movement_direction",	[Input.get_axis('move_left', 'move_right')])
+	mediator.request("set_wants_jump",			[Input.is_action_just_pressed('jump')])
+	mediator.request("set_wants_dash",			[Input.is_action_just_pressed('dash')])
+	mediator.request("set_wants_attack",		[Input.is_action_just_pressed("attack")])
